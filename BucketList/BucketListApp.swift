6b68df78c9ec853f3f4aeecb4aeef08c7f946afc
vlsalina <vincent.salinas.2022@googlemail.com>
2022-09-28 14:17:33 -0700

    First commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Vincent Salinas on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
