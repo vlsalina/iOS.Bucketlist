e7f742a922df87aef1a133c011f027ab3f44ff57
vlsalina <vincent.salinas.2022@googlemail.com>
2022-09-28 16:25:28 -0700

    Finished implement functionality to download data from Wikipedia

---
+++ BucketList/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  BucketList
+//
+//  Created by Vincent Salinas on 9/28/22.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
